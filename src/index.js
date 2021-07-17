7b82713634bda5319b84ff6ed98aa4a0e5928d77
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-07-17 21:42:11 +0300

    new approach

--- src/index.js
+++ src/index.js
@@ -1,44 +1,9 @@
 import "./styles.css";
 
-var note = document.getElementById("a");
-
-var audioClap = new Audio("sounds/clap.wav");
-var audioHihat = new Audio("sounds/hihat.wav");
-var audioKick = new Audio("sounds/kick.wav");
-var audioOpenhat = new Audio("sounds/openhat.wav");
-var audioBoom = new Audio("sounds/boom.wav");
-var audioRide = new Audio("sounds/ride.wav");
-var audioSnare = new Audio("sounds/snare.wav");
-var audioTom = new Audio("sounds/tom.wav");
-var audioTink = new Audio("sounds/tink.wav");
-
-window.addEventListener("keydown", checkKeyPress);
-
-function popNote(el) {
-  el.style.transform = "scale(1.1)";
-  el.style.transition = "0.05s ease-in";
-  el.style.borderColor = "yellow";
-}
-
-function checkKeyPress(key) {
-  if (key.keyCode === 65) {
-    popNote(note);
-    audioClap.play();
-  } else if (key.keyCode === 83) {
-    audioHihat.play();
-  } else if (key.keyCode === 68) {
-    audioKick.play();
-  } else if (key.keyCode === 70) {
-    audioOpenhat.play();
-  } else if (key.keyCode === 71) {
-    audioBoom.play();
-  } else if (key.keyCode === 72) {
-    audioRide.play();
-  } else if (key.keyCode === 74) {
-    audioSnare.play();
-  } else if (key.keyCode === 75) {
-    audioTom.play();
-  } else if (key.keyCode === 76) {
-    audioTink.play();
-  }
-}
+window.addEventListener("keydown", function (e) {
+  console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  // const kei = document.querySelector('.key[key-num="${e.keyCode}"]');
+  if (!audio) return;
+  audio.play();
+});
